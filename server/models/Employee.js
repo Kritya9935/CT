fcc86e6622e145248759476b171740de5865eb8a
Kritya9935 <kashishg832@gmail.com>
2025-03-27 21:27:18 +0530

    first commit

---
+++ server/models/Employee.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const EmployeeSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true }, // Email unique aur required
+    userId: { type: String, required: true, unique: true }, // userId for login
+    password: { type: String, required: true }, // Password required
+    isAdmin: { type: Boolean, default: false } // Admin check, default false
+  },
+  {
+    timestamps: false // No automatic createdAt/updatedAt
+  }
+);
+
+const EmployeeModel = mongoose.model('employees', EmployeeSchema); // Collection name: 'employees'
+module.exports = EmployeeModel;
